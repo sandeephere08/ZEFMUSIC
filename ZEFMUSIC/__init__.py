5ab6bd0f25e5aac421394efbe36da5d1f42b597b
sandeephere08 <sandeepsharma13082003@gmail.com>
2025-04-26 15:40:07 +0530

    Fixed import statement in __init__.py

--- ZEFMUSIC/__init__.py
+++ ZEFMUSIC/__init__.py
@@ -5,7 +5,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Import test configuration
-import test_config as config
+import config
 
 from ZEFMUSIC.core.bot import KINGBot
 from ZEFMUSIC.core.dir import dirr
@@ -33,3 +33,18 @@ Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
+
+from ZEFMUSIC.core.mongo import mongodb
+from ZEFMUSIC.core.call import ZEFMUSICCall
+from ZEFMUSIC.misc import sudo
+from ZEFMUSIC.utils import time_to_seconds
+from ZEFMUSIC.utils.database import get_db
+from ZEFMUSIC.utils.decorators import language
+from ZEFMUSIC.utils.extraction import extract_user
+from ZEFMUSIC.utils.formatters import get_readable_time
+from ZEFMUSIC.utils.inline import help_pannel, play_pannel
+from ZEFMUSIC.utils.inlinequery import inline_query
+from ZEFMUSIC.utils.pastebin import paste
+from ZEFMUSIC.utils.stream import stream
+from ZEFMUSIC.utils.thumbnails import get_thumb
+from ZEFMUSIC.utils.ytdl import ytdl
